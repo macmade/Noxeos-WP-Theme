b13c41d661dc3ec00fde508ee0764e3088d79cd9
macmade <macmade@xs-labs.com>
2014-09-29 07:27:04 +0200

    Cleanup...

--- noxeos-responsive/search.php
+++ noxeos-responsive/search.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<h2 class="tmpl-page-title"><?php printf( 'Results for: «%s»', get_search_query() ); ?></h2>
+<h2 class="text-center"><?php printf( 'Results for: «%s»', get_search_query() ); ?></h2>
 
 <?php if ( have_posts() ) : ?>
 
@@ -8,7 +8,7 @@
 
 <?php else : ?>
 
-<div>
+<div class="alert alert-info">
     Sorry, no page were found.
 </div>
 
