cc1af893fa703e18132f94b8b2d22d1186c56d99
macmade <macmade@xs-labs.com>
2014-09-29 07:25:22 +0200

    Comments...

--- noxeos-responsive/comments.php
+++ noxeos-responsive/comments.php
@@ -44,55 +44,56 @@
 
 $fields = array
 (
-    'author' => '<div class="comment-form-element">'
-             .  '<div class="comment-form-label">'
-             .  '<label for="author">Name:</label>' . ( ( $req ) ? ' <span class="required">*</span>' : '' )
-             .  '</div>'
-             .  '<div class="comment-form-field">'
-             .  '<input id="author" name="author" type="text" value="' . esc_attr( $commenter[ 'comment_author' ] ) . '" size="50"' . $aria_req . ' />'
+    'author' => '<div class="form-group">'
+             .  '<label for="author" class="col-sm-2 control-label"><strong>Name</strong></label>'
+             .  '<div class="col-sm-10">'
+             .  '<input id="author" name="author" type="text" value="' . esc_attr( $commenter[ 'comment_author' ] ) . '" size="50"' . $aria_req . ' class="form-control" placeholder="Your name or nickname (required)" />'
              .  '</div>'
              .  '</div>',
-    'email'  => '<div class="comment-form-element">'
-             .  '<div class="comment-form-label">'
-             .  '<label for="email">Email:</label>' . ( ( $req ) ? ' <span class="required">*</span>' : '' )
-             .  '</div>'
-             .  '<div class="comment-form-field">'
-             .  '<input id="email" name="email" type="text" value="' . esc_attr( $commenter[ 'comment_author_email' ] ) . '" size="50"' . $aria_req . ' />'
+    'email'  => '<div class="form-group">'
+             .  '<label for="email" class="col-sm-2 control-label"><strong>Email</strong></label>'
+             .  '<div class="col-sm-10">'
+             .  '<input id="email" name="email" type="text" value="' . esc_attr( $commenter[ 'comment_author_email' ] ) . '" size="50"' . $aria_req . ' class="form-control" placeholder="Your email address (required)" />'
              .  '</div>'
              .  '</div>',
-    'url'    => '<div class="comment-form-element">'
-             .  '<div class="comment-form-label">'
-             .  '<label for="url">WWW:</label>'
-             .  '</div>'
-             .  '<div class="comment-form-field">'
-             .  '<input id="url" name="url" type="text" value="' . esc_attr( $commenter[ 'comment_author_url' ] ) . '" size="50" />'
+    'url'    => '<div class="form-group">'
+             .  '<label for="url" class="col-sm-2 control-label"><strong>WWW</strong></label>'
+             .  '<div class="col-sm-10">'
+             .  '<input id="url" name="url" type="text" value="' . esc_attr( $commenter[ 'comment_author_url' ] ) . '" size="50" class="form-control" placeholder="Your website URL (optional)" />'
              .  '</div>'
              .  '</div>'
 );
 
+ob_start();
 comment_form
 (
     array(
         'fields'               => apply_filters( 'comment_form_default_fields', $fields ),
-        'comment_field'        => '<div class="comment-form-element">'
-                               .  '<div class="comment-form-label">'
-                               .  '<label for="comment">Comment:</label>'
-                               .  '</div>'
-                               .  '<div class="comment-form-field">'
-                               .  '<textarea id="comment" name="comment" cols="75" rows="8" aria-required="true"></textarea>'
+        'comment_field'        => '<div class="form-group">'
+                               .  '<label for="comment" class="col-sm-2 control-label"><strong>Comment</strong></label>'
+                               .  '<div class="col-sm-10">'
+                               .  '<textarea id="comment" name="comment" cols="75" rows="8" aria-required="true" class="form-control"></textarea>'
                                .  '</div>'
                                .  '</div>',
-        'must_log_in'          => '<div class="must-log-in">' . sprintf( 'You need to <a href="%s">login</a> in order to post a comment', wp_login_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</div>',
-        'logged_in_as'         => '<div class="logged-in-as">' . sprintf( 'Connected as <a href="%1$s">%2$s</a>. <a href="%3$s">Logout</a>', admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</div>',
-        'comment_notes_before' => '<div class="comment-form-notes">Note: your email address won\'t be published on this site.</div>',
+        'must_log_in'          => '<div class="col-sm-2"></div><div class="alert alert-warning text-center col-sm-10 must-log-in">' . sprintf( 'You need to <a href="%s">login</a> in order to post a comment', wp_login_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</div>',
+        'logged_in_as'         => '<div class="col-sm-2"></div><div class="alert alert-success text-center col-sm-10 logged-in-as">' . sprintf( 'Connected as <a href="%1$s">%2$s</a>. <a href="%3$s">Logout</a>', admin_url( 'profile.php' ), $user_identity, wp_logout_url( apply_filters( 'the_permalink', get_permalink( $post_id ) ) ) ) . '</div>',
+        'comment_notes_before' => '<div class="col-sm-2"></div><div class="alert alert-info text-center col-sm-10"><strong>Note:</strong> your email address won\'t be published on this site.</div>',
         'comment_notes_after'  => '',
         'id_form'              => 'commentform',
         'id_submit'            => 'submit',
         'title_reply'          => 'Leave a comment',
         'title_reply_to'       => 'Leave a comment $ %s',
         'cancel_reply_link'    => 'Cancel',
-        'label_submit'         => 'Add the comment',
+        'label_submit'         => 'Leave a comment',
     )
 );
+
+$FORM = str_replace( 'class="comment-form"', 'class="comment-form form-horizontal" role="form"', ob_get_contents() );
+$FORM = str_replace( '<p class="form-submit">', '<p class="col-sm-2"></p><p class="form-submit col-sm-10 text-center">', $FORM );
+$FORM = str_replace( '<input name="submit"', '<input name="submit" class="btn btn-primary"', $FORM );
+
+ob_end_clean();
+
+print $FORM;
 
 ?>
